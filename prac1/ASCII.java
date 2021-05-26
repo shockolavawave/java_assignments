d6e088ac1d47105bd20dd4aee6af96b9f15c2616
Atharv Sambarat <84657415+shockolavawave@users.noreply.github.com>
2021-05-26 16:39:41 +0530

    moved

--- prac1/ASCII.java
+++ prac1/ASCII.java
@@ -4,7 +4,7 @@ public class ASCII
     {
         char c1 = 'a';
         char c2 = 'b';
-
+    
         System.out.println("The ASCII value of " + c1 + " is " + (int) c1);
         System.out.println("The ASCII value of " + c2 + " is " + (int) c2);
     }
