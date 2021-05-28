ce6828af93bbcafe4d9e4e5a38e3cb5f87900f76
Atharv Sambarat <84657415+shockolavawave@users.noreply.github.com>
2021-05-28 22:29:12 +0530

    Update 6.Fabonacci.java

--- Lab_28.5.21/6.Fabonacci.java
+++ Lab_28.5.21/6.Fabonacci.java
@@ -1,10 +1,13 @@
 public class Fabonacci {
     public static void main(String[] args) {
 
-        int i, side = 0;
-        for(i = 0; side<51 ; i++){
-            System.out.println("The size of the side is " + side + " units");
-            side += i;
+        int i;
+        long prev = 0, next = 1, buff;
+        for(i = 0; i<51 ; i++){
+            System.out.println("The size of the side is " + prev + " units");
+            buff = next + prev;
+            next = prev;
+            prev = buff;
         }
     }
 }
