9afef1fc1c63add8919794589fdbcc97fbd0a16f
Atharv Sambarat <84657415+shockolavawave@users.noreply.github.com>
2021-05-31 15:37:29 +0530

    Update 5.Percent.java

--- Lab_28.5.21/5.Percent.java
+++ Lab_28.5.21/5.Percent.java
@@ -11,6 +11,10 @@ public class Percent {
         float percent = ((float)(sub1 + sub2 + sub3 + sub4 + sub5)/max)*100;
 
         System.out.println("The percentage of the five subjects is " + percent + "%");
-
-    }
+        
+        if(percent > 50){
+			System.out.println("The student has passed.");
+		}else{
+			System.out.println("The student has failed.");
+		}    }
 }
